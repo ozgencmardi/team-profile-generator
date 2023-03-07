930b4bbaa8a9d51d903e44defc0dd9f66e96af42
ozgencmardi <ozgencmardi@gmail.com>
2023-03-07 12:47:34 +0000

    index.js updated

--- index.js
+++ index.js
@@ -13,3 +13,38 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+const Employee = require("./lib/Employee");
+
+
+inquirer.prompt([
+    {
+      type: "input",
+      message: "What is the employee's name?",
+      name: "name"
+    },
+    {
+      type: "input",
+      message: "What is the employee's ID?",
+      name: "id"
+    },
+    {
+      type: "input",
+      message: "What is the employee's email address?",
+      name: "email"
+    },
+    {
+      type: "list",
+      message: "What is the employee's role?",
+      name: "role",
+      choices: [
+        "Manager",
+        "Engineer",
+        "Intern"
+      ]
+    }
+  ]).then(answers => {
+    // code to create the appropriate employee object based on the selected role
+  });
+  
+
+const employees = [];
