22440f7153e275b101eea85f649d83f4dd943630
ozgencmardi <ozgencmardi@gmail.com>
2023-03-07 13:44:56 +0000

    index.js updated

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+// TODO: Write Code to gather information about the development team members, and render the HTML file.
+
+const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -10,40 +14,130 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
-const Employee = require("./lib/Employee");
+const employees = [];
 
-const employees = []; // Define the employees array only once, here
-
-inquirer
-  .prompt([
+function promptForManager() {
+  inquirer.prompt([
     {
       type: "input",
       message: "What is the manager's name?",
-      name: "name",
+      name: "name"
     },
     {
       type: "input",
       message: "What is the manager's ID?",
-      name: "id",
+      name: "id"
     },
     {
       type: "input",
       message: "What is the manager's email address?",
-      name: "email",
+      name: "email"
     },
     {
       type: "input",
       message: "What is the manager's office number?",
-      name: "officeNumber",
-    },
-  ])
-  .then((answers) => {
-    const manager = new Manager(
-      answers.name,
-      answers.id,
-      answers.email,
-      answers.officeNumber
-    );
+      name: "officeNumber"
+    }
+  ]).then(answers => {
+    const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
     employees.push(manager);
-    
+    promptForEmployee();
   });
+}
+
+function promptForEmployee() {
+  inquirer.prompt([
+    {
+      type: "list",
+      message: "What is the employee's role?",
+      name: "role",
+      choices: [
+        "Engineer",
+        "Intern",
+        "I don't want to add any more employees"
+      ]
+    }
+  ]).then(answer => {
+    switch (answer.role) {
+      case "Engineer":
+        promptForEngineer();
+        break;
+      case "Intern":
+        promptForIntern();
+        break;
+      default:
+        generateHtml();
+    }
+  });
+}
+
+function promptForEngineer() {
+    inquirer.prompt([
+        {
+          type: "input",
+          message: "What is the engineer's name?",
+          name: "name"
+        },
+        {
+          type: "input",
+          message: "What is the engineer's ID?",
+          name: "id"
+        },
+        {
+          type: "input",
+          message: "What is the engineer's email address?",
+          name: "email"
+        },
+        {
+          type: "input",
+          message: "What is the engineer's GitHub username?",
+          name: "github"
+        }
+      ]).then(answers => {
+        const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+        employees.push(engineer);
+        
+      });
+      
+  
+}
+
+function promptForIntern() {
+    inquirer.prompt([
+        {
+          type: "input",
+          message: "What is the intern's name?",
+          name: "name"
+        },
+        {
+          type: "input",
+          message: "What is the intern's ID?",
+          name: "id"
+        },
+        {
+          type: "input",
+          message: "What is the intern's email address?",
+          name: "email"
+        },
+        {
+          type: "input",
+          message: "What is the intern's school?",
+          name: "school"
+        }
+      ]).then(answers => {
+        const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+        employees.push(intern);
+
+      });
+      
+}
+
+function generateHtml() {
+  const html = render(employees);
+  fs.writeFile(outputPath, html, err => {
+    if (err) throw err;
+    console.log("Team HTML generated successfully!");
+  });
+}
+
+promptForManager();
